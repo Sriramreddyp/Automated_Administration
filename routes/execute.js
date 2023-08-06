dc57a7ee5a94192224080b5d024dc970ca27044c
kratos <sriramreddyp123@gmail.com>
2023-08-06 15:44:03 -0400

    done with intial setup with all bash scripts and utlities included, remained with ansible-playbook routes (some buggy nature should be removed)

--- routes/execute.js
+++ routes/execute.js
@@ -1,108 +1,246 @@
 import Express from "express";
-import * as com from "child_process";
+import util from "node:util";
+import * as com from "node:child_process";
 export const router = Express.Router();
 
+const exec = util.promisify(com.exec);
 
-router.get("/",(req,res)=>{
-res.json({status:"Connected to exec route!!"});
+//Global Variables for a single session
+var hosts = "";
+var groupName = "";
+var keyNamePublic = "id_rsa.pub";
+var keyNamePrivate = "id_rsa";
+var userName = "";
+var rootfilename = "";
+
+router.get("/", (req, res) => {
+  res.json({ status: "Connected to exec route!!" });
 });
-
 
 //utility remoteCommand execution route
-router.post("/commandExec",async (req,res)=>{
- var command = req.body.cmd;
+router.post("/commandExec", async (req, res) => {
+  var command = req.body.cmd;
 
-await com.exec(command,(error,stdout,stderr)=>{
-
-     if(error){
+  await exec(command, (error, stdout, stderr) => {
+    if (error) {
       console.log("error");
       res.send(error.message);
-     }
+    }
 
-     if(stderr){
-        console.log("stdError");
-        res.send(stderr.message);
-       }
+    if (stderr) {
+      console.log("stdError");
+      res.send(stderr.message);
+    }
 
-     console.log("Executed Sucessfully");
-     res.send(stdout);
-});
-});
-
-//considering ssh keys are initially configured.
-//Inventory and ansible set-up route
-//developer defined path -- hardcoded
-//user defined worker node
-//creation of ansible.cfg
-//creation of files dir
-//scp of ssh key to worker node
-//ansible ping from that folder
-//if sucessfull return path,if not return false ,lo error
-router.post("/initial-setup",async (req,res)=>{
-
-var filename = req.body.name;
-var ip = req.body.ip;
-
-//1.
-await com.exec(`mkdir /home/kratos/Ansible/${filename}`,(error,stderr,stdout)=>{
- if(error)
- res.send("Error in creating file for ansible setup");
- else if(stderr)
- res.send("Error in executing command");
- else
- console.log("File Sucessfully Created for setup");
+    console.log("Executed Sucessfully");
+    res.send(stdout);
+  });
 });
 
-//2.
-await com.exec(`cd /home/kratos/Ansible/${filename} && echo [servers] > inventory && echo ${ip} >> inventory && mkdir files`,(error,stderr,stdout)=>{
-if(error)
-res.send("Error in creating inventory file");
-else if(stderr)
-res.send("Error in executing command");
-else
-console.log("Sucessfully created worker node inventory");
+//ssh-key gen route -- (return - keyname)
+// router.post("/ssh-keygen", async (req, res) => {
+//   var keyname = req.body.keyname;
+
+//   await com.exec(
+//     `ssh-keygen -q -t rsa -N '' -f ~/.ssh/${keyname} <<<y >/dev/null 2>&1`,
+//     (error, stderr, stdout) => {
+//       if (error) res.send(error);
+//       else if (stderr) res.send("Error in executing the command..");
+//       else {
+//         keyName = keyname;
+//         console.log(keyName);
+//         res.send("Sucessfully key-pair generated");
+//       }
+//     }
+//   );
+// });
+
+//Initial setup
+//1 -- pull shell and playbooks
+//2 -- Initialize a file for ansible
+//3 -- cp those shell and playbooks to ansible
+//4 -- create inventory and cfg,file,host -- version1
+//5 -- test
+router.post("/initial-setup", async (req, res) => {
+  var workers = req.body.workers;
+  groupName = req.body.groupname;
+  rootfilename = req.body.filename;
+  var usernameBase = req.body.baseuser;
+  var passBase = req.body.pass;
+  userName = req.body.ansibleReqUser;
+
+  //converting array into space seperated string for bash script
+  hosts = " ";
+  for (let i of workers) {
+    hosts = hosts + i + " ";
+  }
+
+  //checking string format
+  console.log(hosts);
+
+  //pull repo
+  async function task1() {
+    let operation = new Promise((resolve, reject) => {
+      exec(
+        `cd /$HOME && git clone git@github.com:Sriramreddyp/Scripts_And_Playbooks.git`,
+        (error, stderr, stdout) => {
+          if (stdout) {
+            console.log("Pulled Repo Sucessfully!!!");
+            resolve(true);
+          }
+          resolve(false);
+        }
+      );
+    });
+    return await operation;
+  }
+
+  //Initialize an ansible File
+  async function task2() {
+    let operation = new Promise((resolve, reject) => {
+      exec(
+        `cd /$HOME/Scripts_And_Playbooks && ./inventory_creation "${hosts}" "${rootfilename}" "${groupName}" "${usernameBase}" "${passBase}"`,
+        (error, stderr) => {
+          if (stderr || error) resolve(false);
+          else {
+            console.log("Sucessfully Created Initial Folder of ansible...");
+            resolve(true);
+          }
+        }
+      );
+    });
+    return await operation;
+  }
+
+  //Setup for user creation
+  async function task3() {
+    let operation = new Promise((resolve, reject) => {
+      exec(
+        `cd /$HOME/Scripts_And_Playbooks && ./manual_user_updation "${hosts}" "${rootfilename}" "${userName}" "${keyNamePublic}"`,
+        (error, stderr) => {
+          if (stderr || error) resolve(false);
+          else {
+            console.log("Setup Sucessfull for user creation...");
+            resolve(true);
+          }
+        }
+      );
+    });
+    return await operation;
+  }
+
+  //Copying playbooks from Scripts file to ansiblefile
+  async function task4() {
+    let operation = new Promise((resolve, reject) => {
+      exec(
+        `cd /$HOME/Scripts_And_Playbooks && cp setting_up_user.yml setting_up_firewall.yml ~/${rootfilename}`,
+        (error, stderr) => {
+          if (stderr || error) resolve(false);
+          else {
+            console.log("File transfer Sucesfull!!!");
+            resolve(true);
+          }
+        }
+      );
+    });
+    return await operation;
+  }
+  let answer = {};
+  let ackOne = await task1();
+  let ackTwo = await task2();
+  let ackThree = await task3();
+  let ackFour = await task4();
+
+  answer = {
+    "Pulling repositories": ackOne,
+    "Ansible - File Setup": ackTwo,
+    "Setup - user creation": ackThree,
+    "Copying Scripts": ackFour,
+  };
+
+  res.json(answer);
 });
 
-//3.
-await com.exec(`cd /home/kratos/Ansible/${filename} && echo "[defaults]" > ansible.cfg && echo "inventory = inventory" >> ansible.cfg && echo "private_key_file = ~/.ssh/id_ed25519" >> ansible.cfg`,(error,stderr,stdout)=>{
-if(error)
- res.send("Error in creating cfg file");
-else if(stderr)
- res.send("Error in executing command");
-else
-res.send(`Initial Setup Sucessfull, path : ${filename}`);
-});
+//Creating User
+//6 -- setup user
+
+//Executed correctly but bugged somewhere - should change later
+router.get("/create-user", async (req, res) => {
+  //Creating User
+  async function task1() {
+    let operation = new Promise((resolve, reject) => {
+      exec(
+        `cd /$HOME/${rootfilename} && ansible-playbook setting_up_user.yml`,
+        () => {
+          console.log("User Creation Sucessfull!!");
+          resolve(true);
+        }
+      );
+    });
+    return await operation;
+  }
+  let answer = {};
+  let ackOne = await task1();
+
+  answer = {
+    "User-creation": ackOne,
+  };
+
+  res.json(answer);
 });
 
-//writing bash scripts for sudo password acess tasks like, ssh key copy, user creation, then only  we can execute other ansible playbooks with user configured as remote one in inventory file
-//Creating a user and giving root acess to that user
+//7 -- setup ssh keys
+//8 -- test again
+router.get("/final-setup", async (req, res) => {
+  //Clear inventory file and update remote_user as username
+  async function task1() {
+    let operation = new Promise((resolve, _reject) => {
+      exec(
+        `cd /$HOME/Scripts_And_Playbooks && ./inventory_refresh "${hosts}" "${rootfilename}" "${groupName}" "${keyNamePrivate}" "${userName}"`,
+        (error, stderr) => {
+          if (stderr || error) resolve(false);
+          else {
+            console.log("SSH Key Updation Sucessfull!!!");
+            resolve(true);
+          }
+        }
+      );
+    });
+    return await operation;
+  }
+  let answer = {};
+  let ackOne = await task1();
 
+  answer = {
+    "SSH Key-Updation": ackOne,
+  };
+
+  res.json(answer);
+});
 
 //Ansible connection test
-router.post("/ansible-test",async(req,res)=>{
-var filename = req.body.name;
-await com.exec(`cd /home/kratos/Ansible/${filename} && ansible all -m ping`,(error,stderr,stdout)=>{
-if(error)
- res.send(error);
-else if(stderr)
- res.send(stderr);
-else
- res.send("Connection sucessfull!!");
-});
-
+router.post("/ansible-test", async (req, res) => {
+  var filename = req.body.name;
+  com.exec(
+    `cd /home/kratos/Ansible/${filename} && ansible all -m ping`,
+    (error, stderr, stdout) => {
+      if (error) res.send(error);
+      else if (stderr) res.send(stderr);
+      else res.send("Connection sucessfull!!");
+    }
+  );
 });
 
 //Task-Executer-playbook
-router.post("/ansible-taskDoEr",async (req,res)=>{
-var filename = req.body.name;
-var task = req.body.task;
-await com.exec(`cd /home/kratos/Ansible/${filename} && ansible-playbook /home/kratos/Ansible/AutomationScripts/setting_up_${task}.yml`,(error,stderr,stdout)=>{
-
-if(error)
- res.send(error);
-else if(stderr)
-  res.send(stderr);
-else
- res.send(`${task} Setup Sucessfull`);
-});
+router.post("/ansible-taskDoEr", async (req, res) => {
+  var filename = req.body.name;
+  var task = req.body.task;
+  await com.exec(
+    `cd /home/kratos/Ansible/${filename} && ansible-playbook /home/kratos/Ansible/AutomationScripts/setting_up_firewall.yml`,
+    (error, stderr, stdout) => {
+      if (error) res.send(error);
+      else if (stderr) res.send(stderr);
+      else res.send(`${task} Setup Sucessfull`);
+    }
+  );
 });
